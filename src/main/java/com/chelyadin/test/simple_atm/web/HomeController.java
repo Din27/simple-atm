9208f16379967b231d4bf03282f7fbcb422eee48
Din <xenon027@gmail.com>
2015-07-04 02:58:12 +0300

    Add logging

--- src/main/java/com/chelyadin/test/simple_atm/web/HomeController.java
+++ src/main/java/com/chelyadin/test/simple_atm/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.chelyadin.test.simple_atm.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController implements ErrorController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     private static final String ERROR_PATH = "/error";
 
     @RequestMapping("/")
     public String home() {
+        logger.info("Home page request");
         return "home";
     }
 
     @RequestMapping(ERROR_PATH)
     public String error() {
+        logger.info("Error page request");
         return "error";
     }
 
