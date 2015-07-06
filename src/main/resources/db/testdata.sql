92329df79afd3eb93424f47bb6b30b1808303dc2
Din <xenon027@gmail.com>
2015-07-07 01:23:15 +0300

    Fix testdata.sql

--- src/main/resources/db/testdata.sql
+++ src/main/resources/db/testdata.sql
@@ -1,4 +1,4 @@
 ---- insert credit cards
-insert into creditcards values ('1111222233334444', '1111', 10000.00);
-insert into creditcards values ('5555666677778888', '5555', 0);
-insert into creditcards values ('1234567812345678', '1234', 999999999.99);
+insert into creditcards (number, pin, amount) values ('1111222233334444', '1111', 10000.00);
+insert into creditcards (number, pin, amount) values ('5555666677778888', '5555', 0);
+insert into creditcards (number, pin, amount) values ('1234567812345678', '1234', 999999999.99);
